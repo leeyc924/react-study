3e501f57f6849973816c5329be691daa845b9c7c
leeyc <leeyc924@naver.com>
2021-01-13 21:31:05 +0900

    konva-exer

---
+++ konva-exer/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layer from './components/Layer';
+
+function App() {
+  return (
+    <>
+      <Layer />
+    </>
+  );
+}
+
+export default App;
---
+++ konva-exer/src/components/Layer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Group,Image} from 'react-konva';
+
+const Layer = () => {
+  return (
+    <>
+    <Group>
+        <Image>
+
+        </Image>
+    </Group>
+    </>
+  );
+};
+
+export default Layer;
